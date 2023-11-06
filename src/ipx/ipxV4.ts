78976efa81b166d4aa6f463ab5426d167516d0f7
Loïc <1536036+zoic21@users.noreply.github.com>
2023-11-06 15:50:20 +0100

    fix

--- src/ipx/ipxV4.ts
+++ src/ipx/ipxV4.ts
@@ -36,7 +36,7 @@ export class IPXV4 implements IpxApiCaller {
         } else if (key.startsWith('VR')) {
           let info = key.replace('VR','').split('-')
           if(info.length == 2){
-            let vrKey = String(info[0]*info[1]).padStart(2, "0")
+            let vrKey = String(parseInt(info[0])*parseInt(info[1])).padStart(2, "0")
             positionByIndex[key] = ipxInfo.data['VR'+vrKey];
           }
         }
