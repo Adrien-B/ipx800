9423b5316c9babf03b0818cef0e5554e239e92e7
Loïc <1536036+zoic21@users.noreply.github.com>
2023-11-06 17:07:57 +0100

    add log

--- src/ipx/ipxV4.ts
+++ src/ipx/ipxV4.ts
@@ -27,6 +27,7 @@ export class IPXV4 implements IpxApiCaller {
     const api = platform.config['api'];
     const url = 'http://' + api.ip + '/api/xdevices.json?key=' + api.key + '&Get=all ';
     return axios.get(url).then(ipxInfo => {
+      platform.log.info(JSON.stringify(ipxInfo.data[key]));
       const positionByIndex = new Map<string, number>();
       Object.keys(ipxInfo.data).map(key => {
         if (key.startsWith('G')) {
@@ -38,6 +39,7 @@ export class IPXV4 implements IpxApiCaller {
           if(info.length == 2){
             let vrkey = 'VR'+String(parseInt(info[0])*parseInt(info[1])).padStart(2, "0");
             positionByIndex[vrkey] = ipxInfo.data[key];
+            platform.log.info('v4------ '+ vrkey + ' value : ' + ipxInfo.data[key]);
           }
         }
       });
