032799ead44eed5da9dba53c9429b7a34a2290af
Loïc <1536036+zoic21@users.noreply.github.com>
2023-11-14 15:47:08 +0100

    bugfix

--- src/ipx/ipxV5.ts
+++ src/ipx/ipxV5.ts
@@ -10,23 +10,22 @@ export class IPXV5 implements IpxApiCaller{
   async getStateByDeviceIndex(platform: IPXPlatform): Promise<Map<string, boolean>> {
     const api = platform.config['api'];
     const url = 'http://' + api.ip + '/api/core/io' + '?ApiKey=' + api.key ;
-    return axios.get(url)
-      .then(ipxInfo => MapUtils.toBoolByNum(ipxInfo.data, '_id', 'on'));
+    return axios.get(url).then(ipxInfo => MapUtils.toBoolByNum(ipxInfo.data, '_id', 'on'));
   }
 
 
   async getAnaPositionByDeviceIndex(platform: IPXPlatform): Promise<Map<string, number>> {
     const api = platform.config['api'];
     const url = 'http://' + api.ip + '/api/core/ana' + '?ApiKey=' + api.key ;
-    return axios.get(url)
-      .then(ipxInfo => {
+    return axios.get(url).then(ipxInfo => {
         const res = MapUtils.toStringByNum(ipxInfo.data, '_id', 'value');
         return res;
-      });
+    });
   }
 
   async setOnDimmer(value: CharacteristicValue, platform: IPXPlatform, accessory: PlatformAccessory) : Promise<AxiosResponse> {
-    return this.setOnRelay(value, platform, accessory);
+    this.setOnRelay(value, platform, accessory);
+    return;
   }
 
   async setOnRelay(value: CharacteristicValue, platform: IPXPlatform, accessory: PlatformAccessory) : Promise<AxiosResponse> {
@@ -36,20 +35,23 @@ export class IPXV5 implements IpxApiCaller{
     if (value as boolean){
       const json = JSON.stringify({ on: true });
       platform.log.error('turning on '+ accessory.context.device.displayName + '  using  ' + url + '  sending  ' + json);
-      return axios.put(url, json);
+      axios.put(url, json);
     } else {
       const json = JSON.stringify({ on: false });
       platform.log.error('turning off '+ accessory.context.device.displayName+ '  using  ' + url + '  sending  ' + json);
-      return axios.put(url, json);
+      axios.put(url, json);
     }
+    return;
   }
 
   async setVRPosition(value: CharacteristicValue, platform: IPXPlatform, accessory: PlatformAccessory) : Promise<AxiosResponse> {
-    return this.setAnaPosition(100 - (value as number), platform, accessory);
+    this.setAnaPosition(100 - (value as number), platform, accessory);
+    return;
   }
 
   async setDimmerPosition(value: CharacteristicValue, platform: IPXPlatform, accessory: PlatformAccessory) : Promise<AxiosResponse> {
-    return this.setAnaPosition(100 - (value as number), platform, accessory);
+    this.setAnaPosition(100 - (value as number), platform, accessory);
+    return;
   }
 
   async setAnaPosition(value: CharacteristicValue, platform: IPXPlatform, accessory: PlatformAccessory) : Promise<AxiosResponse> {
@@ -59,7 +61,8 @@ export class IPXV5 implements IpxApiCaller{
     const json = JSON.stringify({ virtual: true, value: nVal});
     platform.log.error('setting level of '+ accessory.context.device.displayName + ' using  ' + url + '  sending  ' + json);
     platform.log.debug('Set Characteristic Brightness -> ', nVal);
-    return axios.put(url, json);
+    axios.put(url, json);
+    return;
   }
 
 }
